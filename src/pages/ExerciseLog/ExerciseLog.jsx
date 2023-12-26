0c1b3e398ceaeeb2dfb2520ffece6f844e3ef0c7
Tasin007 <mdtasinalam.mta@gmail.com>
2023-12-26 14:56:52 +0600

    exercise data json created and some small modifictions in exercise log

--- src/pages/ExerciseLog/ExerciseLog.jsx
+++ src/pages/ExerciseLog/ExerciseLog.jsx
@@ -1,13 +1,23 @@
 import ExerciseBanner from "./ExerciseBanner";
 
-
 const ExerciseLog = () => {
-    return (
-        <div>
-            <ExerciseBanner></ExerciseBanner>
-            <h2>exerciseLog</h2>
-        </div>
-    );
+  return (
+    <div>
+      <ExerciseBanner></ExerciseBanner>
+      <h2>exerciseLog</h2>
+      <div role="tablist" className="tabs tabs-boxed">
+        <a role="tab" className="tab">
+        Beginner
+        </a>
+        <a role="tab" className="tab tab-active">
+        Intermediate
+        </a>
+        <a role="tab" className="tab">
+        Advanced
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default ExerciseLog;
